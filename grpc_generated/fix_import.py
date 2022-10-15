1f55918fbcfb4969dc061a478a8ee08142767388
n-0 <n-0@github.com>
2022-10-15 23:34:13 +0200

    feat: add autogenerated files

---
+++ grpc_generated/fix_import.py
@@ -0,0 +1,5 @@
+import os
+path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # run in a file
+path = os.path.join(path, 'grpc_generated') # package name where generated files are reside
+file_path = f'{path}/*_pb2*.py*'
+os.system(f'2to3 -wn -f import {file_path}')
